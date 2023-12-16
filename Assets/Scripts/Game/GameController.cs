2ece41dd15d9c14213433f05a3e352c0c51c0f7c
SamuelRodri <samuelrh2001@hotmail.com>
2023-12-16 17:10:59 +0000

    Timer para lanzar la piedra

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -13,16 +13,24 @@ public class GameController : MonoBehaviour
 {
     public int totalPoints;
     public TMP_Text pointsText;
+    public TMP_Text timerText;
     public GameMode gameMode = GameMode.pushing;
+    private bool pushStarted = false;
 
     // Camera
     public float shakeDuration = 0.3f;
     public float shakeStrength = 0.3f;
 
+    // Timer
+    public int pushTime = 3;
+    private int timer;
+
     // Start is called before the first frame update
     void Start()
     {
         pointsText.text = totalPoints.ToString();
+        timerText.text = "";
+        timer = pushTime;
     }
 
     // Update is called once per frame
@@ -32,6 +40,17 @@ public class GameController : MonoBehaviour
         {
             gameMode = GameMode.leaving;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!pushStarted)
+            {
+                pushStarted = true;
+                StartCoroutine("CountDown");
+            }
+        }
+
+        if (timer == 0) gameMode = GameMode.leaving;
     }
 
     public void DestroyElement(DestructibleObject destructible)
@@ -45,5 +64,15 @@ public class GameController : MonoBehaviour
     {
         totalPoints += points;
         pointsText.text = totalPoints.ToString();
+    }
+
+    IEnumerator CountDown()
+    {
+        for(int i = 0; i <= pushTime; i++)
+        {
+            timerText.text = timer.ToString();
+            yield return new WaitForSeconds(1f);
+            timer -= 1;
+        }
     }
 }
